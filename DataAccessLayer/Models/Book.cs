a95524701bd809d2dc4c69d79364c18be52f0850
Sumit Bhattarai <bbhattaraisumit88@gmail.com>
2018-04-27 18:03:12 +0545

    Dll

---
+++ DataAccessLayer/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataAccessLayer
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public DateTime BookedOn { get; set; }
+        public DateTime BookedFor { get; set; }
+        public bool IsCanceled { get; set; }
+        public DateTime CanceledOn { get; set; }
+        public int BookedBy { get; set; }
+        public int BookPilotMappingID { get; set; }
+        public int StaffID { get; set; }
+    }
+}
