d56139f96a3012cfb1b82ca4c4d05a284138e9d3
Sumit Bhattarai <bbhattaraisumit88@gmail.com>
2018-05-04 17:51:16 +0545

    a

--- Paragliding Management System/Program.cs
+++ Paragliding Management System/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Paraglidin_Management_System.Data;
+using Paragliding_Management_System.Identity;
 
 namespace Paragliding_Management_System
 {
@@ -14,7 +17,25 @@ namespace Paragliding_Management_System
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<UsersDbContext>();
+                    DbInitializer.InitializeAsync(context, services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                    //throw;
+                }
+
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- Paragliding Management System/Startup.cs
+++ Paragliding Management System/Startup.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
 using DataAccessLayer.Operations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Paragliding_Management_System.Identity;
+using Paragliding_Management_System.Services;
 
 namespace Paragliding_Management_System
 {
@@ -19,20 +25,51 @@ namespace Paragliding_Management_System
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UsersDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<UsersDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Superuser"));
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
             services.AddMvc();
             // Adds a default in memory implementation of IDistributedCache
             services.AddDistributedMemoryCache();
             services.AddTransient(typeof(BookingDbl));
+
             services.AddSession(options =>
             {
                 // Set a short timeout for easy testing.
                 options.IdleTimeout = TimeSpan.FromDays(1);
                 options.Cookie.HttpOnly = true;
             });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.LoginPath = "/Account/Login"; // If the LoginPath is not set here, 
+                                                      // ASP.NET Core will default to /Account/Login
+                options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here, 
+                                                        // ASP.NET Core will default to /Account/Logout
+                options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is 
+                                                                    // not set here, ASP.NET Core 
+                                                                    // will default to 
+                                                                    // /Account/AccessDenied
+                options.SlidingExpiration = true;
+            });           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +82,7 @@ namespace Paragliding_Management_System
             }
 
             app.UseStaticFiles();
+            app.UseAuthentication();
 
             // Session call
             app.UseSession();
@@ -54,7 +92,7 @@ namespace Paragliding_Management_System
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            });           
         }
     }
 }
