562d748238fdd79e11f142b6b2baf63e7c68b3db
Sumit Bhattarai <bbhattaraisumit88@gmail.com>
2018-05-04 17:46:46 +0545

    User Model Added

---
+++ DataAccessLayer/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace DataAccessLayer.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedOn { get; set; }
+
+    }
+}
