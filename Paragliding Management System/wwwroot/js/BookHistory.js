0c1d9be0024826c69148e3ea2edcbbde70818e74
Abinash <aviz.nepz@gmail.com>
2018-05-08 07:22:06 +0545

    Booking history updated

--- Paragliding Management System/wwwroot/js/BookHistory.js
+++ Paragliding Management System/wwwroot/js/BookHistory.js
@@ -22,7 +22,7 @@
                 type: config.type,
                 contentType: config.contentType,
                 cache: config.cache,
-                url: config.method,
+                url: window.location.origin + '/' + config.method,
                 data: config.data,
                 dataType: config.dataType,
                 success: config.successMethod,
@@ -34,87 +34,15 @@
         function ajaxFailure(error) {
             console.log(error);
         };
-        function loadCart() {
-            if (typeof (Storage) !== "undefined") {
-                if (localStorage.cartVar) {
-                    $cartArr = JSON.parse(localStorage.cartVar);
-                    var html = '', i = 0, len = $cartArr.length;
-                    for (i; i < len; i++) {
-                        html += '<tr id="tr' + $cartArr[i].StaffID + '"><td>' + $cartArr[i].StaffName + '</td><td>' + $cartArr[i].BookedFor + '</td><td><a class="btn btn-danger" id="remCart" href="javascript:void(0)" data-stafid=' + $cartArr[i].StaffID + '><i class="glyphicon glyphicon-trash"></i>Remove From Cart</a></td></tr>';
-                    }
-                    $bukDivTable.html(html);
-                }
-            } else {
-                messageDisplay("Sorry, your browser does not support web storage...", "error");
-            }
-        };
         function other() {
-            //loadBooking();
-            $bukDiv.on('click', '#createBooking', function () {
-                var currentUrl = window.location.pathname;
-                var redirectUrl = $("#RedirectTo").val() + '?ReturnUrl=' + currentUrl;
-                if (localStorage.cartVar !== '[]') {
-                    var i = 0, len = $cartArr.length, BookedBy = '', StaffID = '', BookedFor = '';
-                    for (i; i < len; i++) {
-                        BookedFor = $cartArr[0].BookedFor;
-                        StaffID += $cartArr[i].StaffID + ',';
-                    }
-                    if (userIDTemp === "") {
-                        window.location.href = redirectUrl;
-                    } else {
-                        saveBooking(0, userIDTemp, StaffID, BookedFor);
-                    }
-                } else {
-                    messageDisplay('Cart Is Empty', 'error');
-                }
-            });
+            loadBooking();
             $bukDiv.on('click', '#cancel-book', function () {
                 var bookID = $(this).attr('data-bookid');
                 cancelBooking(bookID);
             });
-            $bukDiv.on('click', '#remCart', function () {
-                var staffID = $(this).attr('data-stafid');
-                var i = 0, len = $cartArr.length;
-                for (i; i < len; i++) {
-                    if ($cartArr[i].StaffID && $cartArr[i].StaffID === staffID) {
-                        $cartArr.splice(i, 1);
-                        break;
-                    }
-                }
-                $bukDivTable.find('#tr' + staffID).remove();
-                localStorage.cartVar = JSON.stringify($cartArr);
-            });
-
-            function saveBooking(BookID, BookedBy, StaffID, BookedFor) {
-                config.type = "POST";
-                config.method = "api/Book/Save";
-                config.data = JSON.stringify({
-                    BookID: BookID,
-                    BookedFors: BookedFor,
-                    BookedBy: BookedBy,
-                    StaffIDs: StaffID
-                });
-                config.successMethod = saveBookingSuccess;
-                config.errorMethod = ajaxFailure;
-                config.contentType = "application/json",
-                    ajaxCall(config);
-            };
-            function saveBookingSuccess(data) {
-                var res = data;
-                switch (res) {
-                    case 0:
-                        loadBooking();
-                        messageDisplay('Pilot Booked Successfully', 'success');
-                        break;
-                    case 1:
-                        messageDisplay('Pilot Already Booked For This Date', 'success');
-                        break;
-                    default:
-                }
-            };
             function loadBooking() {
                 config.type = "GET";
-                config.method = "api/Book/All";
+                config.method = "api/Book/History";
                 config.data = '';
                 config.successMethod = loadBookingSuccess;
                 config.errorMethod = ajaxFailure;
@@ -127,8 +55,8 @@
                 var i = 0, len = data.length;
                 for (i; i < len; i++) {
                     html += '<tr><td>';
-                    html += data[i].users.userName;
-                    html += '</td><td>';
+                    //html += data[i].users.userName;
+                    //html += '</td><td>';
                     html += data[i].firstName;
                     html += '</td><td>';
                     html += data[i].bookedOn;
@@ -179,7 +107,6 @@
             };
         };
         $this.init = function () {
-            loadCart();           
             other();
         };
     }
